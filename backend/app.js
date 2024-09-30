6c1e117e143be1719e2529b9382220a63ecc398d
boombot11 <sahilsanskruti11@gmail.com>
2024-09-30 19:55:11 +0530

    added mongodb support

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require('express')
 const app=express()
 const cors=require('cors')
@@ -7,19 +8,20 @@ const router=require('./routes/route')
 app.use(express.static('public'));
 app.use(express.json())
 app.use(cors())
+const API_KEY=process.env.MONGO_KEY
+mongoose.set('strictQuery',false);
 
-// mongoose.set('strictQuery',false);
-// mongoose.connect("YOUR KEY")
-// .then(()=>{
-//     console.log("connected to db");
+mongoose.connect(`mongodb+srv://boombot11:${API_KEY}@test.md8hana.mongodb.net/`)
+.then(()=>{
+    console.log("connected to db");
 
-// }).catch((error)=>
-// {
-// console.log(error);
-// }
-// )
+}).catch((error)=>
+{
+console.log(error);
+}
+)
 
-app.use('/',router)
+app.use('/',router);
 
 app.listen(7080,()=>{
     console.log("running");
